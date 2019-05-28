597b75e50540cca6c06a7a0a967255da0a9df6ea
Luiz Felipe <felipe.maciel@lsbd.ufc.br>
2019-05-28 10:28:30 -0300

    Improved toBytes to handle more than one byte

--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 8
+#include <time.h>
+#define CEIL_BY_8(n) n / 8 + (n % 8 ? 1 : 0)
 
-typedef unsigned char byte;
+typedef unsigned char BYTE;
 
-byte* toBytes(byte *_nums, int n) {
-    byte *bytes = calloc(n / 8, sizeof(byte));
+BYTE* randBinArray(int n) {
+
+    BYTE *binStr = malloc(n * sizeof(BYTE));
+    for(int i=0; i < n; i++)
+        sprintf(&binStr[i], "%d", rand() % 2);
+
+    return binStr;
+}
+
+BYTE* toBytes(BYTE *_nums, int n) {
+    BYTE *bytes = calloc(CEIL_BY_8(n), sizeof(BYTE));
     if(bytes != NULL) {
         for(int b=0; b < n; b++) {
             bytes[b / 8] <<= 1;
             bytes[b / 8] |= _nums[b] == '1' ? 1 : 0;
         }
+
+        bytes[n / 8] <<= 8 - (n % 8);
 
         return  bytes;
     }
@@ -18,7 +30,7 @@ byte* toBytes(byte *_nums, int n) {
     return NULL;
 }
 
-void printArray(byte *arr, int n) {
+void printArray(BYTE *arr, int n) {
 
     for(int i=0; i < n; i++) {
         if(i % 16 == 0 && i != 0)
@@ -29,13 +41,14 @@ void printArray(byte *arr, int n) {
 
 int main() {
 
-    byte nums[SIZE] = "11110000";
+    srand(time(NULL));
+    int n = 1 + rand() % 256;
+    BYTE *binStr = randBinArray(n);
 
-    printArray(nums, SIZE);
-    printf("\n\n");
+    printf("s    = %s\nn(s) = %d\nb(s) = %d\n\n", binStr, n, CEIL_BY_8(n));
 
-    byte *bytes = toBytes(nums, SIZE);
-    printArray(bytes, SIZE / 8);
+    BYTE *bytes = toBytes(binStr, n);
+    printArray(bytes, CEIL_BY_8(n));
     printf("\n");
 
     return EXIT_SUCCESS;
