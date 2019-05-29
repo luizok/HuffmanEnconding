10177c6dfb24bd005d21a1ebd7cdc4b1ca773b52
Luiz Felipe <luizfelipesmaciel@gmail.com>
2019-05-29 07:32:38 -0300

    testing heap

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "minheap.h"
+#include "heapnode.h"
 #define SIZE 8
 
 typedef unsigned char byte;
@@ -37,6 +39,19 @@ int main() {
     byte *bytes = toBytes(nums, SIZE);
     printArray(bytes, SIZE / 8);
     printf("\n");
+
+//    HeapNode_t *n1 = newHeapNode('C', 100);
+//    HeapNode_t *n2 = newHeapNode('B', 50);
+//    HeapNode_t *n3 = newHeapNode('A', 2);
+//    MinHeap_t *heap = createMinHeap(3);
+//
+//    insertHeapNode(heap, n1);
+//    insertHeapNode(heap, n2);
+//    insertHeapNode(heap, n3);
+//
+//    printMinHeap(heap);
+//    extractMin(heap);
+//    printMinHeap(heap);
 
     return EXIT_SUCCESS;
 }
